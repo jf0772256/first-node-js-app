5c06fb3387a10127b1c10a08bb0daef1bc12d188
Jesse Fender <jf0772256@otc.edu>
2018-12-11 17:49:29 -0600

    haha! it works!

--- server/src/app.js
+++ server/src/app.js
@@ -16,9 +16,9 @@ app.get('/status', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    res.send({
-        message: 'Hello ${req.body.email}! Your account has been established! Have lots of fun!'
-    })
+  res.send({
+    message: `Hello ${req.body.email}! Your account has been established! Have lots of fun!`
+  })
 })
 
 app.listen(process.env.PORT || 8081)
