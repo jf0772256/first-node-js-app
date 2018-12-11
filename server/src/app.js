5844474bb6183ebb152f46f46732221f09e70850
Jesse Fender <jf0772256@otc.edu>
2018-12-10 18:20:21 -0600

    build of the start of the server...
    localhost:8081/status

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,16 @@
 
-console.log('hello world')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send('hello world!')
+})
+// process.env.PORT ||
+app.listen(8081)
