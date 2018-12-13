496349909d924f1a2b0b903361726cb648a1d677
Jesse Fender <jf0772256@otc.edu>
2018-12-13 17:30:20 -0600

    fixed issues found by eslint

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,8 @@
 
-import axios from "axios"
+import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL:'http://localhost:8081'
+    baseURL: 'http://localhost:8081'
   })
 }
