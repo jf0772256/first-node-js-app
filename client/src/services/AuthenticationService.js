19c277605c9b5c5bbf5c4c193ce997dd71be2cf6
Jesse Fender <jf0772256@otc.edu>
2018-12-13 17:30:02 -0600

    renamed and fixed issues found by eslint

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,8 +2,8 @@
 import Api from '@/services/Api'
 
 export default {
-  register (credentials){
-    return Api().post('register',credentials)
+  register (credentials) {
+    return Api().post('register', credentials)
   }
 }
 
