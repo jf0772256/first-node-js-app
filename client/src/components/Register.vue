27d20197e5e948a59cdded5b71e853c38beb9118
Jesse Fender <jf0772256@otc.edu>
2018-12-13 16:57:44 -0600

    Created vue page component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="text" name="email" placeholder="email" id="email" autocomplete="off"><br>
+    <input type="password" name="password" id="password" autocomplete="off">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
