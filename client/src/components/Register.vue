35a71bf3e68cfa1e105a26f3ada9dfbc8749257f
Jesse Fender <jf0772256@otc.edu>
2018-12-13 17:31:54 -0600

    set up the connection with teh server
    connects with the data from register and returns a json string to object
    for use with furture notifies

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,17 +2,31 @@
   <div>
     <h1>Register</h1>
 
-    <input type="text" name="email" placeholder="email" id="email" autocomplete="off"><br>
-    <input type="password" name="password" id="password" autocomplete="off">
+    <input type="text" name="name" id="name" placeholder="name" v-model="name"><br>
+    <input type="text" name="email" placeholder="email" id="email" autocomplete="off" v-model="email"><br>
+    <input type="password" name="password" id="password" autocomplete="off" v-model="password"><br>
+    <button name="submit" id="submit" @click="register">Register</button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  name: 'Register',
   data () {
     return {
-      //
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        name: this.name,
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
     }
   }
 }
