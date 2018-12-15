2d0fda5e8d8148927b9371a13f2d1a00a4805b31
Jesse Fender <jf0772256@otc.edu>
2018-12-14 18:40:54 -0600

    moved routes to sep folder, instanced sequelize

--- server/src/app.js
+++ server/src/app.js
@@ -3,22 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'hello world!'
-  })
-})
+require('./routes')(app)
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.name}! Your username is ${req.body.email}. Your account has been established! Have lots of fun!`
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server has started on port ${config.port}.`)
   })
-})
-
-app.listen(process.env.PORT || 8081)
